5b70d6fe0c6f34ae20bfe28b994f08889486cfe2
peri <periarta2004@gmail.com>
2024-10-28 22:39:48 +0800

    First Commit

---
+++ src/server/handler.js
@@ -0,0 +1,41 @@
+const predictClassification = require("../services/inferenceService");
+const crypto = require("crypto");
+const storeData = require("../services/storeData");
+
+async function postPredictHandler(request, h) {
+  const { image } = request.payload;
+  const { model } = request.server.app;
+  const { confidenceScore, label, explanation, suggestion } =
+    await predictClassification(model, image);
+  const id = crypto.randomUUID();
+  const createdAt = new Date().toISOString();
+
+  const data = {
+    id: id,
+    result: label,
+    explanation: explanation,
+    suggestion: suggestion,
+    confidenceScore: confidenceScore,
+    createdAt: createdAt,
+  };
+
+  // Panggil storeData dan tangani jika terjadi error
+  try {
+    await storeData(id, data);
+  } catch (error) {
+    console.error("Error storing data:", error);
+  }
+
+  const response = h.response({
+    status: "success",
+    message:
+      confidenceScore > 99
+        ? "Model is predicted successfully."
+        : "Model is predicted successfully but under threshold. Please use the correct picture",
+    data,
+  });
+  response.code(201); // Pastikan ini adalah integer, yaitu 201
+  return response;
+}
+
+module.exports = postPredictHandler;
